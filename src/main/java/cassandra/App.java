5a455baffdc5974ea06feaab3ee578a3153db460
mrryko <mystadniksasha@gmail.com>
2018-12-17 16:13:40 +0200

    Initial commit

---
+++ src/main/java/cassandra/App.java
@@ -0,0 +1,35 @@
+package cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class App {
+
+    private static final String ContactPoints = "35.180.61.204";
+    private static final int Port = 9042;
+
+
+    public static void main(String[] args){
+
+        Cluster cassandraCluster = Cluster
+                .builder()
+                .addContactPoint(ContactPoints)
+                .withPort(Port)
+                .build();
+
+        Session session = cassandraCluster.connect();
+
+        var allHosts = session.getCluster().getMetadata().getAllHosts();
+        allHosts.forEach((k) -> System.out.println(k.getAddress().toString()));
+
+        System.out.println("______________");
+
+        var keyspaces = session.getCluster().getMetadata().getKeyspaces();
+
+        for(var keyspace:keyspaces){
+            System.out.println(keyspace.getName());
+            keyspace.getTables().forEach((k) -> System.out.println(k.getName()));
+        }
+
+    }
+}
